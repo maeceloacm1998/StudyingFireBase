aa432bf286c51c3f9c2fe1098b30282f3a0f6430
Marcelo Antonio <maeceloacm1998@gmail.com>
2021-06-28 22:33:38 -0300

    initial studying fire base, aula 1 complete

--- App.tsx
+++ App.tsx
@@ -1,12 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import firebase from "./src/service/firebaseConnection";
+
+// Desativar avisos de yarn
+console.disableYellowBox = true;
 
 export default function App() {
+  const [nome, setNome] = useState("carregando...");
+
+  async function pegaNome() {
+    {
+      /*Esse cara aqui se chama Olheiro, ele e responsavel por pegar em tempo real 
+      toda alteracao que acontecer no banco de dados. E recomendavel que nao utilize
+      com excesso, para nao comprometer a performace
+
+      --> Essa reqwuisicao do fire base esta pegando um no do meu banco de dados
+      */
+    }
+    await firebase
+      .database()
+      .ref("nome")
+      .on("value", (snapshot) => {
+        setNome(snapshot.val());
+      });
+
+    {
+      /*Esse, diferente do olheiro, pega apenas uma vez o valor no banco de dados,
+      nao atualizando de acordo com a mudanca
+      */
+    }
+    await firebase
+      .database()
+      .ref("nome")
+      .once("value", (snapshot) => {
+        setNome(snapshot.val());
+      });
+
+    {
+      /*Esse, diferente do olheiro, pega apenas uma vez o valor no banco de dados,
+      nao atualizando de acordo com a mudanca
+
+      --> Essa requisicao esta pegando o dado em um diretorio passado pelo ref
+      */
+    }
+    await firebase
+      .database()
+      .ref("usuarios/1/nome")
+      .once("value", (snapshot) => {
+        setNome(snapshot.val());
+      });
+
+    {
+      /*Esse, diferente do olheiro, pega apenas uma vez o valor no banco de dados,
+      nao atualizando de acordo com a mudanca
+
+      --> Essa requisicao esta pegando o index 1 inteiro
+      */
+    }
+    await firebase
+      .database()
+      .ref("usuarios/1")
+      .once("value", (snapshot) => {
+        setNome(snapshot.val().idade);
+      });
+  }
+
+  useEffect(() => {
+    pegaNome();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{nome}</Text>
     </View>
   );
 }
@@ -14,8 +81,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/service/firebaseConnection.ts
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQqm_PIc_g0kduSUeMC49Kl-HO70p0eoQ",
+  authDomain: "meuap-272ec.firebaseapp.com",
+  databaseURL: "https://meuap-272ec-default-rtdb.firebaseio.com",
+  projectId: "meuap-272ec",
+  storageBucket: "meuap-272ec.appspot.com",
+  messagingSenderId: "999629152134",
+  appId: "1:999629152134:web:d915fccc39f6cf8833d607",
+  measurementId: "G-GGDNH60ZVV",
+};
+
+if (!firebase.apps.length) {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
